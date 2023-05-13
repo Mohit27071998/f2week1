a31e95eab7b90afde42d8e5b34ed6dfcf3e9942f
Mohit Kamdi <mohitkamdi317@gmail.com>
2023-05-13 17:24:52 +0530

    update

--- script.js
+++ script.js
@@ -8,20 +8,56 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map((student)=>{
+    if(student.marks > 50){
+      console.log(student);
+    }
+  })
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+   
+  arr.forEach((student)=>{
+    if(student.marks > 50){
+      console.log(student);
+    }
+  })
+    
 }
 
 function addData() {
   //Write your code here, just console.log
+  let stud = {
+    id:4,
+    name:"susan",
+    age:"20",
+    marks:45
+  }
+  arr.push(stud);
+  console.log(arr);
 }
+
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let res=arr.filter((student)=>{
+    if(student.marks<50){
+      return false;
+    }else{
+      return true;
+    }
+  })
+  console.log(res);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let arr1 = [
+    { id: 5, name: "wick", age: "16", marks: 78 },
+    { id: 6, name: "sparrow", age: "21", marks: 95 },
+    { id: 7, name: "uchiha", age: "22", marks: 34 },
+  ];
+  let ans = arr.concat(arr1);
+  console.log(ans)
 }
